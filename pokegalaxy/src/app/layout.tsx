30fdec1d44bbaa78e81acd7c403ef0dfc74bf817
Jiahui Wang <jiawang539@hotmail.com>
2025-08-28 11:48:14 +1200

    [feat] initial setup

---
+++ pokegalaxy/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Orbitron } from "next/font/google";
+import "./globals.css";
+
+const orbitron = Orbitron({
+  variable: "--font-orbitron",
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pokemon Galaxy",
+  description: "Explore every Pokémon in a cosmic journey",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${orbitron.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ pokegalaxy/src/app/pokedex/page.tsx
@@ -0,0 +1,18 @@
+const PokedexPage = () => {
+  return (
+    <div className="flex items-center min-h-screen p-20 flex-col">
+      <div className="w-full max-w-2xl flex flex-col items-center gap-10">
+        <p className="font-orbitron text-6xl font-bold neon-text-outline text-center">Pokédex</p>
+        <input
+          name="search"
+          type="text"
+          placeholder="Search Pokémon..."
+          className="mt-6 p-2 border border-gray-300 rounded w-full h-14 text-white"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-6 mt-10"></div>
+    </div>
+  );
+};
+
+export default PokedexPage;
